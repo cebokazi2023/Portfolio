8ad4140f3616205eb99dfd32c828a80e2bbf6d0a
cebokazi2023 <125352732+cebokazi2023@users.noreply.github.com>
2023-02-28 11:28:29 +0200

    Add files via upload

---
+++ Portfolio/main.js
@@ -0,0 +1,41 @@
+
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click',()=>{
+            nav.addclassList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nam-menu')
+
+
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n=> n.addEventListener('click', linkAction))
+
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll' scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelectorAll('.nav_menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('')
+        }
+    })
+}
+
